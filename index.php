106bd82ae3ac3a0e011f2700ae5a8cb48dbdae5f
Aluno <aluno@edu.senac.br>
2018-11-14 21:46:49 -0200

    modificação

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php
+		$nome = "Geovane Lindo";
+			echo $nome; //Comentário, esse serve para apenas uma linha
+			/*outro comentário
+			esse serve para comentar um bloco*/
+			#comentário, esse também serve para comentar uma linha apenas
+		?>
+	</h1>
+
+</body>
+</html>
