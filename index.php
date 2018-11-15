d2c1f131c3563a6b51652455446d900b2c9a5a5e
Aluno <aluno@edu.senac.br>
2018-11-14 22:05:47 -0200

    colocando variavel em ação

--- index.php
+++ index.php
@@ -7,11 +7,20 @@
 	<h1>
 		<?php
 		$nome = "Geovane Lindo";
-			echo $nome; //Comentário, esse serve para apenas uma linha
+		$nascimento = "08/01/1993";
+		$sobre_nome = "Costa";
+			/*echo $nome; //Comentário, refere-se a variável nome
+			echo '<br />'
+			echo $nascimento;
+			echo '<br />'
+			echo $sobre_nome;*/
 			/*outro comentário
 			esse serve para comentar um bloco*/
 			#comentário, esse também serve para comentar uma linha apenas
 		?>
+		<p><?php echo $nome; ?></p>
+		<p><?php echo $sobre_nome; ?></p>
+		<p><?php echo $nascimento; ?></p>
 	</h1>
 
 </body>
